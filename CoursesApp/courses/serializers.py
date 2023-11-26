5e1a38234ffb53ede1762071a9da5bce9013a041
sinhhungahihi <sinhhungmaths@gmail.com>
2023-11-26 17:14:50 +0700

    lap 26/11/2023

--- CoursesApp/courses/serializers.py
+++ CoursesApp/courses/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Course, Category
+from rest_framework import serializers
+from .models import Course, Category, Tag
+
+class TagSerializers(ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id','name']
 
 class CategorySerializers(ModelSerializer):
     class Meta:
@@ -7,6 +13,16 @@ class CategorySerializers(ModelSerializer):
         fields = '__all__'
 
 class CourseSerializers(ModelSerializer):
+    tags = TagSerializers(many=True)
+    image = serializers.SerializerMethodField(source='image')
     class Meta:
         model = Course
-        fields = ['id', 'subject', 'create_date', 'category']
+        fields = ['id', 'subject', 'image', 'create_date', 'category', 'tags']
+
+
+    def get_image(self, course):
+        if(course.image):
+            request = self.context.get('request')
+            if(request):
+                return request.build_absolute_uri('/static/%s' % course.image.name)
+            return '/static/%s' % course.image.name
--- CoursesApp/courses/views.py
+++ CoursesApp/courses/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import serializers
 from .models import Course, Category
 from .serializers import CourseSerializers, CategorySerializers
 
@@ -8,7 +11,23 @@ from .serializers import CourseSerializers, CategorySerializers
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializers
+
 class CourseViewSet(viewsets.ModelViewSet):
-    queryset = Course.objects.filter(active = True)
+    queryset = Course.objects.filter(active = True).all()
     serializer_class = CourseSerializers
 
+    def get_queryset(self):
+        queries = self.queryset
+        q = self.request.query_params.get('q')
+        if q:
+            queries = queries.filter(subject__icontains=q)
+
+        cate_id = self.request.query_params.get('category_id')
+        if cate_id:
+            queries = queries.filter(category_id=cate_id)
+        return queries
+    @action(methods=['get'],detail=True)
+    def lessons(self, request, pk):
+        lessons = self.get_object().lesson_set.filter(active=True)
+        return Response(serializers.LessonsSerializers(
+            lessons, many=True, context={'request': request}).data, status = status.HTTP_200_OK)
